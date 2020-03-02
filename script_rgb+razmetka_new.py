1bc73213f7e0e13bca5b3474efbc0bdad275cf93
apatros-dessel <44582213+apatros-dessel@users.noreply.github.com>
2020-03-02 06:40:20 +0300

    Add files via upload

--- script_rgb+razmetka_new.py
+++ script_rgb+razmetka_new.py
@@ -48,13 +48,6 @@ for id in ids_list:
 print('Filtered process of length %i' % len(proc))
 proc.get_vector_cover(r'resursp_filtered_cover.json')
 
-''' FIND SCENES FOR OBJECTS '''
-
-input_list = []
-qual_dict = {}
-full_cloud = []
-error_list = []
-
 def intersect_array(shp1, shp2):
     ds1, lyr1 = geodata.get_lyr_by_path(shp1)
     if lyr1 is None:
@@ -77,9 +70,9 @@ def intersect_array(shp1, shp2):
 
 def layers_intersection_array(shapes_list, proc):
     scenes_list = []
-    result_array = None
     export_array = None
     for shp1 in shapes_list:
+        result_array = None
         for ascene in proc.scenes:
             if result_array is None:
                 result_array = intersect_array(shp1, ascene.datamask())
@@ -94,15 +87,119 @@ def layers_intersection_array(shapes_list, proc):
                 export_array = np.vstack([export_array, result_array])
     return export_array
 
+def infullcloud(id):
+    return int(id in globals()['full_cloud'])
+
+''' FIND SCENES FOR OBJECTS '''
+
+input_list = []
+qual_dict = {}
+full_cloud = []
+error_list = []
+
+
+
 intersection_array = layers_intersection_array(obj_of_interest, proc)
 
-scene_ids = proc.get_ids()
+scene_ids = np.array(proc.get_ids())
 
 scene_dates = []
 for ascene in proc.scenes:
     scene_dates.append(ascene.meta.datetime)
 scene_dates = np.array(scene_dates)
 
+for i in range(intersection_array.shape[0]):
+
+    feat_id_arr = scene_ids[intersection_array[i,:]]
+    feat_dates_arr = scene_dates[intersection_array[i,:]]
+
+    sort = np.argsort(feat_dates_arr)[::-1]
+    feat_id_arr = feat_id_arr[sort]
+    feat_dates_arr = feat_dates_arr[sort]
+
+    scroll(feat_id_arr, header='Id list for feature %i:' % i)
+
+    clouded_list = []
+    approved = False
+
+    for id in feat_id_arr:
+        # print(1, id)
+        if id in input_list:
+            approved = True
+            print('Scene already in input_list: {}'.format(id))
+            break
+        elif id in qual_dict:
+            if qual_dict.get(id):
+                approved = True
+                print('Scene previously approved: {}'.format(id))
+                break
+        else:
+            try:
+                with Image.open(proc.get_scene(id).quicklook()) as quicklook:
+                    quicklook.show()
+                    qualtest = input('Print 0 if scene is of bad quality or 10 if scene is covered with clouds: ')
+                    if qualtest not in [0, 10]:
+                        qual_dict[id] = True
+                        input_list.append(id)
+                        approved = True
+                        print('Scene appended to input_list: {}'.format(id))
+                        break
+                    elif qualtest == 10:
+                        clouded_list.append(id)
+                    elif qualtest == 0:
+                        qual_dict[id] = False
+            except:
+                print('Error making quicklook for {}'.format(id))
+
+    if not approved:
+        if len(clouded_list) == 0:
+            error_list.append(i)
+            print('Unable to find proper scenes for feature {}'.format(i))
+        else:
+            scroll(clouded_list, header='Cannot find proper scene, include clouded scenes too:')
+            input_list.extend(clouded_list)
+            full_cloud.extend(clouded_list)
+
+scroll(input_list, header='Final input list:')
+
+scroll(full_cloud, header='Full cloud list:')
+
+scroll(error_list, header='Errors:')
+
+report_dict = OrderedDict()
+for id in input_list:
+    ascene = proc.get_scene(id)
+    report_dict[id] = {
+        'fullpath': ascene.fullpath,
+        'date': ascene.meta.name('[date]'),
+        'filepath': ascene.get_band_path('red')[0],
+        'datamask': ascene.datamask(),
+        'quicklook': ascene.quicklook(),
+        'clouds': infullcloud(id),
+    }
+
+dict_to_xls(path2xls, report_dict)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+proc_new = process()
+for id in input_list:
+    proc_new.scenes.append(proc.get_scene(id))
+
+print('%i scenes ready for making rgb' % len(proc_new))
+
+'''
 
 for path2shp in obj_of_interest:
 
@@ -228,3 +325,5 @@ for id in input_list:
     proc_new.scenes.append(proc.get_scene(id))
 
 print('%i scenes ready for making rgb' % len(proc_new))
+
+'''

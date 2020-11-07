cfd2042c42901f9292d71a05ba5411950b4d9963
Sadkov_SA <apatros@yandex.ru>
2020-11-07 03:38:50 +0300

    [RASTER] Stripping raster

--- test_66_strip_raster.py
+++ test_66_strip_raster.py
@@ -1,6 +1,6 @@
 from geodata import *
 
-folder = r''
+folder = r'\\172.21.195.215\thematic\products\s3\resursp'
 
 files = folder_paths(folder,1,'tif')
 
@@ -9,7 +9,9 @@ check_list = []
 for file in files:
     res = StripRaster(file, compress='DEFLATE')
     if res==0:
-        checklist.append(split3(file)[1])
+        name = split3(file)[1]
+        print('File stripped: %s' % name)
+        checklist.append(name)
 
 if check_list:
     with open(fullpath(folder,'stripped.txt'),'w') as txt:

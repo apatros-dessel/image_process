5f032c6260cbfac1cdee7fed1b9c070c45937346
apatros-dessel <44582213+apatros-dessel@users.noreply.github.com>
2020-11-18 12:24:50 +0300

    [StripRaster] Improving test_66_strip_raster for better performance

--- test_66_strip_raster.py
+++ test_66_strip_raster.py
@@ -1,17 +1,18 @@
 from geodata import *
 
-folder = r'd:\rks\destrip_test\ETRIS.RP1.GTNL1.4949.5.0.2014-05-14.L0.NTSOMZ_MSK.NTSOMZ_MSK'
+folder = r'\\172.21.195.215\thematic\products\s3\resursp'
 
 files = folder_paths(folder,1,'tif')
-
 checklist = []
 
-for file in files:
-    res = StripRaster(file, compress='DEFLATE')
-    if res==0:
-        name = split3(file)[1]
-        print('File stripped: %s' % name)
-        checklist.append(name)
+if files:
+    print('START STRIPPING %i FILES' % len(files))
+    for i, file in enumerate(files):
+        res = StripRaster(file, compress='DEFLATE')
+        if res==0:
+            name = split3(file)[1]
+            print('%i FILE STRIPPED: %s' % (i, name))
+            checklist.append(name)
 
 if checklist:
     with open(fullpath(folder,'stripped.txt'),'w') as txt:

775c4545f2d239ed29fcb2c17b0e7b2fb3a5cdea
apatros-dessel <44582213+apatros-dessel@users.noreply.github.com>
2021-02-18 11:03:06 +0300

    [GetFromDB] Test for uploading Kanopus data from S3 database

--- test_80_getfromdb.py
+++ test_80_getfromdb.py
@@ -1,6 +1,7 @@
 from tools import *
+import pandas as pd
 
-txt_path = r'C:\Users\admin\Downloads\snow2.txt'
+index_path = r'\\172.21.195.2\thematic\!SPRAVKA\S3\16\snowy.xlsx'
 dout = None
 final_dir = r'e:\rks\qltest'
 suredir(final_dir)
@@ -19,21 +20,35 @@ class FolderDirs(dict):
                         continue
                 self[name] = path
 
-with open(txt_path) as txt:
-    for line in txt.read().split('\n'):
-        id = ExtractKanId(line)
-        if dout is None:
-            path0 = tempname()
-        else:
-            path0 = dout
-            suredir(path0)
-        command = r'''gu_db_query -w "source='kanopus' and hashless_id='%s'" -d %s''' % (id, path0)
-        os.system(command)
-        dirs = FolderDirs(path0)
-        for folder in dirs:
-            imgid = ExtractKanId(folder)
-            raster = folder_paths(dirs[folder],1,'tif')[0]
-            raster_fin = fullpath(final_dir,imgid,'tif')
-            shutil.copyfile(raster, raster_fin)
-            print('WRITTEN: %s\n' % id)
-        destroydir(path0)
+if index_path.endswith('txt'):
+    with open(index_path) as txt:
+        lines = txt.read().split('\n')
+elif index_path.endswith('xls'):
+    lines = []
+    for xls_line in xls_to_dict(index_path):
+        id_source = xls_line.get('ID')
+        if id_source:
+            lines.append(id_source)
+elif index_path.endswith('xlsx'):
+    lines = []
+    for id_source in pd.read_excel(index_path).get('ID'):
+        if id_source:
+            lines.append(id_source)
+
+for line in lines:
+    id = ExtractKanId(line)
+    if dout is None:
+        path0 = tempname()
+    else:
+        path0 = dout
+        suredir(path0)
+    command = r'''gu_db_query -w "source='kanopus' and hashless_id='%s'" -d %s''' % (id, path0)
+    os.system(command)
+    dirs = FolderDirs(path0)
+    for folder in dirs:
+        imgid = ExtractKanId(folder)
+        raster = folder_paths(dirs[folder],1,'tif')[0]
+        raster_fin = fullpath(final_dir,imgid,'tif')
+        shutil.copyfile(raster, raster_fin)
+        print('WRITTEN: %s\n' % id)
+    destroydir(path0)

6672c82f66e89ba1e694ea4810fa691cee7f313b
apatros-dessel <44582213+apatros-dessel@users.noreply.github.com>
2021-06-09 11:33:50 +0300

    [razmetka] Create masks from Rita's folder structure

--- test_81_razmetka.py
+++ test_81_razmetka.py
@@ -1,6 +1,6 @@
 from razmetka import *
 
-path = r'\\172.21.195.2\THEMATIC\!razmetka\Resurs_geoton\Resurs_geoton_snow'
+path = r'\\172.21.195.2\THEMATIC\!razmetka\Kanopus\Kanopus_artifacts'
 datacats = ['img', 'img_check', 'shp_hand', 'shp_auto', 'mask']
 
 folder_index = MaskTypeFolderIndex(path, datacats=datacats)
@@ -8,12 +8,12 @@ folder_index = MaskTypeFolderIndex(path, datacats=datacats)
 #scroll(folder_index.subtypes, header='subtypes', lower = '\n')
 # scroll(folder_index.Images(subtype='', bandclass='MS', datacat='img'))
 
-# folder_index.UpdateFromMS('PAN','img_mist_cloud_shadow_surface',use_source_pms=False)
+# folder_index.UpdateFromMS('PMS','check',use_source_pms=False)
 # sys.exit()
 for subtype in folder_index.subtypes:
     # print(subtype, CorrectFolderName(subtype, ['img', 'shp_hand', 'shp_auto', 'mask']))
-    folder_index.UpdateFromMS('PMS', subtype, use_source_pms=False)
-    # folder_index.SaveBandsSeparated(subtype, datacat=datacat)
+    # folder_index.UpdateFromMS('PMS', subtype, use_source_pms=False)
+    folder_index.SaveBandsSeparated(subtype, datacat='img')
     # print(subtype, ' : ', folder_index.Images(subtype=subtype, bandclass='BANDS', datacat='img'))
     pass
 # folder_index.ReprojectPanToMs(r'e:\rks\kantest2\img_cloud_mist_shadow', 'img_cloud_mist_shadow')

1cf618ff5ad90dd9c4913901cc11f52db2c7545e
apatros-dessel <44582213+apatros-dessel@users.noreply.github.com>
2021-07-12 10:35:16 +0300

    [backmask] Correct tiling raster and vector bands

--- test_101_backmask.py
+++ test_101_backmask.py
@@ -1,10 +1,11 @@
 from razmetka import *
 
-img_folder_out = r'e:\rks\razmetka\set085__20210705__resurs_kshmsa_vr_ms_clouds\images'
+img_folder_out = r'e:\rks\razmetka\set090__20210706__resurs_kshmsa_vr_clouds_bands_nir2\images\resurs'
 msk_in = r'e:\test\tiles'
-img_in = r''
-msk_folder_out = r''
-msk_match_folder = r''
+img_tiles_folder = r'e:\test\tiles_img'
+# img_in = r'e:\rks\razmetka\set087__20210706__resurs_kshmsa_vr_clouds_bands_green\images\resurs'
+msk_folder_out = r'e:\rks\razmetka\set090__20210706__resurs_kshmsa_vr_clouds_bands_nir2\masks\full\resurs'
+msk_match_folder = r'e:\test\basic'
 
 def GetMaskOut(msk_in):
     final = OrderedDict()
@@ -21,27 +22,38 @@ def TileImage(img_folder, msk_data):
     del_list = []
     for file in folder_paths(img_folder, 1, 'tif'):
         folder, name, tif = split3(file)
-        if FindAny(name, ['_red', 'green', '_blue', '_nir', '_nir1', '_nir2'], False):
-            id = '_'.join(name.split('_')[:-1])
+        if FindAny(name, ['_red', '_green', '_blue', '_nir', '_nir1', '_nir2'], False):
+            parts = name.split('_')
+            id = '_'.join(parts[:-1])
+            color = '_' + parts[-1]
         else:
             id = name
+            color = ''
         # print(id)
         if id in msk_data:
             for grn in msk_data[id]:
                 msk = msk_data[id][grn]
-                name_out = '%s.GRN%s' % (name, grn)
-                img_out = fullpath(folder, name_out, 'tif')
-                # BackMask(msk, file, img_out)
-                RepairImage(msk, img_out, 1, band_order = [1])
-                print('MASKED: %s' % name_out)
-        del_list.append(name)
+                img_name = '%s.GRN%s%s' % (id, grn, color)
+                img_out = fullpath(folder, img_name, tif)
+                if os.path.exists(img_out):
+                    continue
+                else:
+                    # BackMask(msk, file, img_out)
+                    # print(['', '_red', '_green', '_blue', '_nir1', '_nir2'].index(color))
+                    # sys.exit()
+                    RepairImage(msk, img_out, 1, band_order = [['', '_red', '_green', '_blue', '_nir1', '_nir2'].index(color)])
+                    print('MASKED: %s' % img_name)
+            delete(file)
+            del_list.append(name)
     return del_list
 
-def TileMask(msk_folder, msk_match_folder, msk_data, msk_out_folder):
+def TileMask(msk_folder, msk_match_folder, msk_data):
     del_list = []
     msk_files = folder_paths(msk_folder,1,'tif')
     msk_match_folder = folder_paths(msk_match_folder,1,'tif')
     msk_match_names = flist(msk_match_folder, Name)
+    # scroll(msk_files)
+    # sys.exit()
     for msk_in in msk_files:
         folder, name, tif = split3(msk_in)
         if FindAny(name, ['_red', 'green', '_blue', '_nir', '_nir1', '_nir2'], False):
@@ -55,16 +67,22 @@ def TileMask(msk_folder, msk_match_folder, msk_data, msk_out_folder):
             if os.path.getsize(msk_in)==os.path.getsize(msk_match_folder[msk_match_names.index(id)]):
                 for grn in msk_data[id]:
                     msk_name = '%s.GRN%s%s' % (id, grn, color)
-                    msk_out = fullpath(msk_out_folder, name, tif)
-                    copyfile(msk_data[id], msk_out)
-                    print('COPY MASK: %s' % name)
+                    msk_out = fullpath(folder, msk_name, tif)
+                    if os.path.exists(msk_out):
+                        continue
+                    else:
+                        copyfile(msk_data[id][grn], msk_out)
+                        print('COPY MASK: %s' % name)
+                delete(msk_in)
                 del_list.append(name)
             else:
                 print('RASTER MASK MISMATCH: %s' % name)
     return del_list
 
-msk_data = GetMaskOut(msk_in)
-# del_list = TileImage(img_in, msk_data)
-# del_list = TileMask(msk_folder, msk_match_folder, msk_data, msk_out_folder)
-
+img_data = GetMaskOut(img_tiles_folder)
+del_list = TileImage(img_folder_out, img_data)
 scroll(del_list, header='Raster to delete:')
+
+msk_data = GetMaskOut(msk_in)
+del_list = TileMask(msk_folder_out, msk_match_folder, msk_data)
+scroll(del_list, header='Mask to delete:')

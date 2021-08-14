1668e547f58fb4b4a822cf9a09fb8dbb64122e47
Sadkov_SA <apatros@yandex.ru>
2021-08-14 22:25:32 +0300

    [#use] Using test code

--- test_56_unite_vector.py
+++ test_56_unite_vector.py
@@ -1,6 +1,6 @@
 from geodata import *
 
-pin = r'\\172.21.195.2\FTP-Share\ftp\s3\resursp'
-pout = r'\\172.21.195.2\FTP-Share\ftp\s3\resursp\fullcover.shp'
+pin = r'e:\rks\source\BAD_KV6_11936_09137_00_KANOPUS_20210220'
+pout = r'e:\rks\source\fullcover.shp'
 
 unite_vector(folder_paths(pin,1,'json'), pout)

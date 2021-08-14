753b78a105eab816f9abb6871c97947ef0ff5e7e
Sadkov_SA <apatros@yandex.ru>
2021-08-14 22:29:05 +0300

    [ScriptFolder] Script Folder

--- test_94_folder_structure.py
+++ test_94_folder_structure.py
@@ -1,4 +1,174 @@
-from tools import *
+from geodata import *
+
+object_indices = [
+    'o', # original
+    '&c\d+', # cut
+    '&ca\d+', # cut mask
+    '&q\d+',  # quicklook
+    '#s', # substandard
+    '#d', # original degree
+]
+
+bandnames = ['_red', '_green', '_blue', '_nir']
+
+raster_size = {
+    'RP': {'MS': 2.1, 'PAN': 0.7, 'PMS': 0.7},
+}
+
+class WrongNameError(RuntimeError):
+
+    def __init__(self, *args, **kwargs):  # Wrong file name template
+        pass
+
+def IndexFolder(folder_name, folder_path):
+    for tmpt in globals()['folder_names']:
+        if re.search(tmpt, folder_name):
+            func = globals()['folder_names'][tmpt](folder_path)
+            ids = {}
+            for name in os.listdir(folder_path):
+                id = func(name)
+                if id in ids:
+                    ids[id].append(name)
+                else:
+                    ids[id]
+            return ids
+
+def IndexCutMask(name):
+    if name.lower().endswith('shp'):
+        if '_cut' in name:
+            id = name.split('_cut')[0]
+            return id
+        else:
+            raise WrongNameError()
+
+def IndexCutImg(name):
+    if name.lower().endswith('tif'):
+        if '_cut' in name:
+            id = name.split('_cut')[0]
+            return id
+        else:
+            raise WrongNameError()
+
+def IndexQuicklook(name):
+    if name.lower().endswith('tif'):
+        search = re.search('_Q?L?\d+\.\d*m&\.tif^', name)
+        if search:
+            id = name[:-1*(len(search.group()))]
+            return id
+        else:
+            raise WrongNameError()
+
+def IndexOriginalDegree(name):
+    if name.lower().endswith('tif'):
+        if '_deg' in name:
+            id = name.split('_deg')[0]
+            return id
+        else:
+            raise WrongNameError()
+
+folder_names = {
+    '$&cut_shp^': ('&cut_shp', IndexCutMask),
+    '$&cut_img^': 'c',
+    '$&\d+m^': 'q',
+    '$#substandard^': '#s',
+    '$#original_deg^': '#d',
+}
+
+def ParseName(name):
+    if re.search(r'^RP.+GEOTON', name):
+        sat = 'RP'
+        idsearch = re.search(r'^RP.+SCN\d+', name)
+        if idsearch:
+            id = idsearch.group()
+            ending = name[len(id):].split('.')
+            if len(ending)>=3:
+                return sat, ending[1], ending[2], id
+            elif len(ending)==2:
+                return sat, ending[1], '', id
+            else:
+                return sat, '', '', id
+        else:
+            return sat,'','',name
+    else:
+        raise WrongNameError(name)
+
+class SceneObject(OrderedDict):
+
+    def __init__(self, path):
+        f, name, e = split3(path)
+        assert e in ['shp', 'tif']
+        self.path = path
+        self.folder = f
+        self.name = name
+        self['ext'] = e
+        if name.endswith('_border'):
+            assert e == 'shp'
+            name = name[:-7]
+            self['border'] = True
+        cutsearch = re.search('_cut\d+',name)
+        self['cutend'] = ''
+        while cutsearch:
+            cutend = cutsearch.group()
+            name = name[:-1*len(cutend)]
+            self['cutend'] += cutend
+            cutsearch = re.search('_cut\d+', name)
+        self['band'] = ''
+        for band in globals()['bandnames']:
+            if name.endswith(band):
+                self['band'] = band
+                name = name[:-1*len(band)]
+        qlsearch = re.search('_\d+\.?\d*m$', name)
+        if qlsearch:
+            self['ql'] = qlsearch.group()
+
+        else:
+            self['ql'] = ''
+        self['sat'], self['type'], self['level'], self['baseid'] = ParseName(name)
+        self.CheckRasterParams()
+
+    test_94_folder_structure.py
+    def CheckRasterParams(self):
+        advice = []
+        if self['ext'] == 'tif':
+            if self['cutend']:
+                border_path = r'%s/&cut_shp/%s_border.shp' % (self.folder, self.name)
+                if not os.path.exists(border_path):
+                    print('CUT BORDER NOT FOUND: %s' % border_path)
+                    advice.append('Make border')
+            if os.path.exists(self.path):
+                raster = gdal.Open(self.path)
+                if raster:
+                    if self.IndexRasterCount() != raster.RasterCount:
+                        print('WRONG RASTER COUNT: %s %i' % (self.path, raster.RasterCount))
+                        advice.append('Fix raster count')
+                    if raster.GetSpatialRef().IsGeographic():
+                        print('DEGREE DATA WHERE UTM ANTICIPATED: %s' % self.path)
+                        advice.append('Convert to UTM')
+                    else:
+                        if 0.9 < abs( self.IndexRasterPixelSize() / raster.GetGeoTransform()[1] ) < 1.1:
+                            pass
+                        else:
+                            print('WRONG RASTER PIXEL SIZE: %s' % self.path)
+                            advice.append('Fix pixel size')
+                else:
+                    print('CANNOT OPEN RASTER: %s' % self.path)
+            else:
+                print('RASTER NOT FOUND: %s' % self.path)
+
+    def IndexRasterCount(self):
+        if self['band']:
+            return 1
+        elif self['type']=='PAN':
+            return 1
+        elif self['sat'] in ['RP']:
+            return 4
+
+    def IndexRasterPixelSize(self):
+        if self['ql']:
+            ql_str = self['ql']
+            return float(re.search('\d+\.?\d*', ql_str).group())
+        else:
+            return globals()['raster_size'][self['sat']][self['type']]
 
 class FolderDirs(OrderedDict):
 
@@ -11,7 +181,7 @@ class FolderDirs(OrderedDict):
                         continue
                     self[name] = path
 
-def ScriptFolder(corner):
+def ScriptFolder(corner, extensions=None):
     folders = FolderDirs(corner)
     if folders:
         result = OrderedDict()
@@ -19,6 +189,43 @@ def ScriptFolder(corner):
             result[name] = ScriptFolder(folders[name])
         return result
     else:
-        return len(folder_paths(corner, 1, ['shp', 'tif']))
+        return len(folder_paths(corner, 1, extensions))
 
-scroll(ScriptFolder(r'\\172.21.195.2\thematic\!razmetka\database'), empty=False)
+# scroll(ScriptFolder(r'\\172.21.195.2\thematic\!razmetka\database'), empty=False)
+
+class ImageFolder(OrderedDict):
+
+    def __init__(self, img_folder):
+        self.type = os.path.split(img_folder)
+        self.subtypes = ''
+
+class ImageSubtype(OrderedDict):
+
+    def __init__(self, img_folder):
+        self.corner = img_folder
+        self.folder_names = globals()['folder_names']
+        if not os.path.exists(img_folder):
+            raise FileNotFoundError
+        names = os.listdir(img_folder)
+        names.reverse()
+        for name in names:
+            path = '%s/%s' % (img_folder, name)
+            if os.path.isfolder(path):
+                assert FolderDirs(path) is None
+                IndexFolder(name, path)
+            else:
+                id, ext = os.path.splitext(name)
+                if ext.lower() == 'tif':
+                    self.NewID(id)
+
+
+        self.subtype = os.path.split(img_folder)
+
+    def NewID(self, id):
+        if id in self:
+            print('ID EXISTS: %s' % id)
+        else:
+            self[id] = {}
+
+for file in folder_paths(r'\\172.21.195.2\thematic\!razmetka\Resurs_geoton\!Resurs_Geoton\MS\img\img_cloud',1,'tif'):
+    obj = SceneObject(file)

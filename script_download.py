77315ac012620ca7a27b057f79ca700a02b37256
apatros-dessel <44582213+apatros-dessel@users.noreply.github.com>
2021-09-03 11:07:10 +0300

    [script_download] Download original NTSOMZ PAN & MS data (server connection required)

--- script_download.py
+++ script_download.py
@@ -1,9 +1,17 @@
 from tools import *
+import argparse
 from progress.bar import IncrementalBar
 
-folder_in = r'y:\102_2020_33'
-folder_out = r'\\172.21.195.160\thematic\S3_NATAROVA_\102_2020_33'
+parser = argparse.ArgumentParser()
+parser.add_argument('folder_in', help='Папка с исходными данными')
+parser.add_argument('folder_out', help='Папка для копирования')
+parser.add_argument('--checksum', default=True, dest='checksum', help='Проверять контрольные суммы (по умолчанию - True)')
+args = parser.parse_args()
+folder_in = args.folder_in
+folder_out = args.folder_out
+checksum = boolstr(args.checksum)
 
+print('\nИдёт поиск файлов в %s' % folder_in)
 input_folders, input_files = folder_paths(folder_in)
 cut_len = len(folder_in)
 
@@ -22,7 +30,7 @@ def DownloadAndCheck(ifile, ofile, iter_lim = 10, checksum = True):
             shutil.copyfile(ifile, ofile)
             iter += 1
     else:
-        while os.path.getsize(file1) != os.path.getsize(file2):
+        while os.path.getsize(ifile) != os.path.getsize(ofile):
             if iter > iter_lim:
                 print(' ITERATION NUMBER EXEEDED: %s' % Name(ifile))
                 return None
@@ -30,6 +38,22 @@ def DownloadAndCheck(ifile, ofile, iter_lim = 10, checksum = True):
             shutil.copyfile(ifile, ofile)
             iter += 1
     return os.path.getsize(ofile)
+
+print('\nОбнаружено %i файлов, начать копирование:\n из: \t%s\n в: \t%s\n%s ? (y/n)' % (len(input_files), folder_in, folder_out, [' без проверки контрольных сумм', ' с проверкой контрольных сумм'][checksum]) )
+
+while True:
+    i = 0
+    try:
+        if i >= 10: sys.exit()
+        start = boolstr(input(' >>>'))
+    except:
+        i += 1
+    else:
+        # print('\n')
+        break
+
+if not start:
+    sys.exit()
 
 for ifolder in input_folders:
     if '.PMS' in ifolder:
@@ -41,7 +65,6 @@ for ifolder in input_folders:
         else:
             os.makedirs(ofolder)
 
-print('\nSTART %s\n' % os.path.split(folder_in)[1])
 copy_size = 0
 exists_size = 0
 miss_size = 0
@@ -54,14 +77,14 @@ for ifile in input_files:
     if '.PMS' in ofile:
         miss_size += os.path.getsize(ifile)
     elif os.path.exists(ofile):
-        size = DownloadAndCheck(ifile, ofile)
+        size = DownloadAndCheck(ifile, ofile, checksum = checksum)
         if size is not None:
             exists_size += size
         else:
             error_size += os.path.getsize(ifile)
     else:
         shutil.copyfile(ifile, ofile)
-        size = DownloadAndCheck(ifile, ofile)
+        size = DownloadAndCheck(ifile, ofile, checksum = checksum)
         if size is not None:
             copy_size += size
         else:

268a5c3774073a5e60f00e2429b14e700f2ed7c2
apatros-dessel <44582213+apatros-dessel@users.noreply.github.com>
2021-10-13 10:49:45 +0300

    [rgb] Add value limits for Kanopus and Resurs-Geoton

--- test_27_rgb.py
+++ test_27_rgb.py
@@ -1,10 +1,13 @@
 from geodata import *
 from datetime import datetime
 
-files = folder_paths(r'd:\rks\Irkutsk\rgb\Landsat',1,'tif')
-suredir(r'd:\rks\Irkutsk\rgb_8bit_fix2\Landsat')
+folder_in = r'\\172.21.195.2\exchanger\Popov\Хайлову_11_10_21'
+files = folder_paths(folder_in,1,'tif',filter_folder=['out','OUT'])
+end_folder = 'd:\\test\\'
+suredir(r'd:\test\SR+PMS')
 for path_in in files:
-    path_out = r'd:\rks\Irkutsk\rgb_8bit_fix2\Landsat\%s_8bit_1proc.tif' % Name(path_in)
+    path_out = end_folder + path_in[len(folder_in):]
+    suredir(os.path.split(path_out)[0])
     t = datetime.now()
     RasterToImage3(path_in,
                                path_out,
@@ -12,10 +15,12 @@ for path_in in files:
                                # band_limits = [(0.02, 0.98), (0.02, 0.98), (0.02, 0.98)],
                                band_limits=[(0.006, 0.9998), (0.006, 0.9998), (0.006, 0.9998)],
                                # band_limits=[(2, 4), (2, 4), (2, 4)],
-                               data_limits=[(6000, 15000), (6000, 15000), (6000, 15000)], # Landsat RGB
+                               # data_limits=[(6000, 15000), (6000, 15000), (6000, 15000)], # Landsat RGB
                                # data_limits=[(500, 5500), (200, 4200), (200, 4200)], # Sentinel SWIR
                                # data_limits=[(6000, 21000), (6000, 21000), (6000, 21000)], # Landsat SWIR
                                # data_limits=[(200, 1800), (200, 1800), (200, 1800)], # Sentinel RGB
+                               # data_limits=[(100, 2700), (100, 2700), (100, 2700)],  # Kanopus Resurs RGB
+                               data_limits=[(100, 750), (100, 750), (100, 750)],  # Kanopus Resurs RGB
                                gamma=0.85,
                                exclude_nodata=True,
                                enforce_nodata=0,
